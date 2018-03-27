095c5d4bd4a8187a7f87a84abd1d2d7da3064369
sinkyuho <bb7305@naver.com>
2018-03-27 10:54:20 +0900

    [2017-03-28] Test.js #1

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,15 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Cloude Servixe';
+var text = 'this is a text';
+text = 10.5;
+var a = 33.7;
+var b = a*text;
+//alert(text);
+var iceCream = 'chocolate';
+/*if (iceCream === 'icecream') {
+  alert('icecrean');    
+} else {
+  alert('chocolate');    
+}*/
+var id = prompt('ID 입력',' ');
+alert(id+ '가 로그인함.') ;
